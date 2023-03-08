e807ef857deef7235e22154329ac7bfc54be90f1
Christopher Haralampoudis <christopherharalampoudis@ChristophersMBP.lan>
2023-03-07 19:44:49 -0800

    hitting open ai from input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,6 @@
 import { H1 } from "@/components/shared/typography";
 
-export async function getData() {
-  const res = await fetch(`http://localhost:3000/api/hello`);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-}
-
-export default async function Page() {
-  const data = await getData();
-  console.log(data)
-
+export default function Page() {
   return (
     <H1 className="bg-gradient-to-r from-sky-400 to-slate-600 bg-clip-text text-transparent">
       TripIt
--- pages/api/prompt.js
+++ pages/api/prompt.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 export default async function handler(req, res) {
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Hello world",
+        prompt: req.body,
     });
     res.status(200).json({ text: completion.data.choices[0].text });
 }
