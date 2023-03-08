c25ca8c3bdd02fb34fc32b358fdb70b34467a745
Sam Hamburger <sam@sensorum.ai>
2023-03-08 08:56:46 -0500

    format and add package

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,43 +1,51 @@
-'use client';
+"use client";
 import { useState, useRef } from "react";
 import { Textarea } from "@/components/shared/ui/Textarea";
 import { H2 } from "@/components/shared/typography";
 import { Button } from "@/components/shared/ui/Button";
 
 export async function getData(text: string) {
-    const res = await fetch(`${window.location.origin}/api/prompt`, {method: 'POST', body: text});
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-  
-    // Recommendation: handle errors
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data');
-    }
-  
-    return res.json();
+  const res = await fetch(`${window.location.origin}/api/prompt`, {
+    method: "POST",
+    body: text,
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
 export default function Input() {
-    const [response, setResponse] = useState(null);
+  const [response, setResponse] = useState(null);
 
-    const inputRef = useRef<HTMLTextAreaElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
-    const onClick = async () => {
-        console.log(inputRef);
-        if (inputRef && inputRef.current && inputRef.current.value) {
-            const data = await getData(inputRef.current.value);
-            console.log(data);
-            setResponse(data.text);
-        }
-    };
+  const onClick = async () => {
+    console.log(inputRef);
+    if (inputRef && inputRef.current && inputRef.current.value) {
+      const data = await getData(inputRef.current.value);
+      console.log(data);
+      setResponse(data.text);
+    }
+  };
 
-    return (
-      <div className="rounded-md bg-slate-800 p-6">
-        {response && <H2 className="mb-4">{response}</H2>}
-        <Textarea className="mb-4" placeholder="Type your message here." ref={inputRef} />
-        <Button className="float-right" onClick={() => onClick()}>Generate</Button>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="rounded-md bg-slate-800 p-6">
+      {response && <H2 className="mb-4">{response}</H2>}
+      <Textarea
+        className="mb-4"
+        placeholder="Type your message here."
+        ref={inputRef}
+      />
+      <Button className="float-right" onClick={() => onClick()}>
+        Generate
+      </Button>
+    </div>
+  );
+}
--- pages/api/prompt.js
+++ pages/api/prompt.js
@@ -1,14 +1,14 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY,
+  apiKey: process.env.OPEN_AI_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: req.body,
-    });
-    res.status(200).json({ text: completion.data.choices[0].text });
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: req.body,
+  });
+  res.status(200).json({ text: completion.data.choices[0].text });
 }
